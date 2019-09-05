fa4002fbbe5c8b4ea702fedcaffd8d633c127589
christopherleveston <christopherleveston@outlook.com>
2019-09-05 15:05:26 -0500

    initial commits

---
+++ main.py
@@ -0,0 +1,82 @@
+import mysql.connector
+
+# Add a function for user to enter host, user, and password of connection.------------
+askForConnection = "Connect to your database:"
+hostname = input("Host: ")
+username = input("Username: ")
+password = input("Password: ")
+
+# VARIABLES
+mydb = mysql.connector.connect(host=hostname, user=username, passwd=password)
+mycursor = mydb.cursor()
+
+question = """\n
+Type a number to execute a command
+1 - Create Database, 2 - Use ~'db name'~, 3 - Show Databases, 4 - Exit
+"""
+askTables = """\n
+1 - SHOW TABLES, 2 - SHOW DATABASES, 3 - Exit
+"""
+
+# FUNCTIONS
+
+
+def showDatabases():
+	presentDB = "\nHere are your current databases."
+	print(presentDB.upper())
+	mycursor.execute("SHOW DATABASES")
+	for x in mycursor:
+		print(x)
+
+
+def exitSQL():
+	mycursor.execute("exit()")
+
+
+def ask():
+	choice = input(question.upper())
+	if choice == "1":
+		askDBName = "What is the name of your new database?"
+		dbName = input(askDBName.upper())
+		mycursor.execute("CREATE DATABASE " + dbName)
+		print(dbName + " is now a database!")
+		ask()
+		
+
+	elif choice == "2":
+		whichToUse = "Which database to use?"
+		nowUsing = "You're now using, "
+
+		dbUse = input(whichToUse.upper())
+		mycursor.execute("USE " + dbUse)
+
+		print(nowUsing + dbUse.upper())
+		usingQuestions = input(askTables)
+	
+	elif choice == "3":
+		exitSQL()
+
+		if usingQuestions == "1":
+			mycursor.execute("SHOW TABLES")
+			for x in mycursor:
+				print(x)
+			ask()
+
+		elif usingQuestions == "2":
+			showDatabases()
+			ask()
+
+		elif usingQuestions == "3":
+			exitSQL()
+
+		else:
+			ask()
+
+	elif choice == "3":
+		showDatabases()
+		ask()
+
+
+showDatabases()
+ask()
+
